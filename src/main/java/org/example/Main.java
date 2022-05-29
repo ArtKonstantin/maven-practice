40ed154f9f36c5751c50227129b7ed93337aa36f
ArtKonstantin <artkonstantin1987@gmail.com>
2022-05-29 17:59:23 +0300

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import com.github.javafaker.Faker;
-
 public class Main {
     public static void main(String[] args) {
-        Faker faker = new Faker();
-        String card = faker.finance().creditCard();
-        System.out.println("card = " + card);
     }
 }
